d8ad131c1822d71f195370dd1b60657d7ac275a9
K0G1408 <144092723+K0G1408@users.noreply.github.com>
2025-04-08 09:59:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def predict():
     resultado_ia = IA.predict(img_array)
     return jsonify({"resultado": resultado_ia})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
 
